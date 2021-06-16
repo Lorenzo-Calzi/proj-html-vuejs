3ca35ddef63db41bdea4aca93dcae8615f8d845a
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-06-16 11:21:48 +0200

    Aggiunti dati header e footer tramite VueJS

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,6 +7,60 @@
   \********************/
 /***/ (() => {
 
+var app = new Vue({
+  el: "#app",
+  data: {
+    headerList: [{
+      li: 'Courses'
+    }, {
+      li: 'Curse Formats'
+    }, {
+      li: 'Add Course'
+    }, {
+      li: 'Pages'
+    }, {
+      li: 'Demos'
+    }],
+
+    /* headerIcon: [
+        { i: <i class="fab fa-linkedin"></i>},
+        { i: <i class="fab fa-instagram"></i>},
+        { i: <i class="fab fa-facebook"></i>},
+        { i: <i class="fab fa-twitter"></i>},
+    ], */
+    footerAbout: [{
+      p: 'Masterstudy is Education WordPress theme featured by learning'
+    }, {
+      p: 'Management System (LMS) for online education.'
+    }, {
+      p: 'Developed by StylemixThemes'
+    }],
+    footerContact: [{
+      p: 'USA, California 20, FIrst Avenue, California'
+    }, {
+      p: 'Tel.: +1 212 458 300 32'
+    }, {
+      p: 'Fax: +1 212 375 24 14'
+    }, {
+      p: 'info@masterstudy.com'
+    }],
+    footerList: [{
+      li: 'Blog'
+    }, {
+      li: 'Home'
+    }, {
+      li: 'Shortcodes'
+    }, {
+      li: 'Courses'
+    }, {
+      li: 'Membership A...'
+    }, {
+      li: 'Typography'
+    }]
+  },
+  methods: {},
+  mounted: function mounted() {}
+});
 var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
 var x = setInterval(function () {
   var now = new Date().getTime();
